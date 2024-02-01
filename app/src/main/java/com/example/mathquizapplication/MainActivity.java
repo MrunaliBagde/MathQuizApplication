8a1cf3408cf7d73987279d6a8ce8314eaf569156
Mrunali Bagde <bagdemrunali525@gmail.com>
2024-02-01 20:47:09 +0530

    feat: Created main activity with layout activity_main

---
+++ app/src/main/java/com/example/mathquizapplication/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.mathquizapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {  `                                                                                                         1qqqqqqqqqqqqqq
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
